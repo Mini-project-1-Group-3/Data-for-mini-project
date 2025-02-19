a2062babb3b4b77bde0c2d83c6e1333fbbfca6e9
Ilyas Rehouni <ilyas.rehouni@gmail.com>
2025-02-19 10:11:55 +0100

    Two hours of work

--- sqlcode_v4.sql
+++ sqlcode_v4.sql
@@ -1,37 +1,4 @@
-DROP TABLE IF EXISTS dim_happiness, dim_country, dim_series, dim_gdp, indicators;
-
-
-CREATE TABLE IF NOT EXISTS dim_happiness (
-	"country_id" VARCHAR(255),
-	"year" INT,
-	"log_gdp" NUMERIC(6,3),
-	"social_support" NUMERIC(6,3),
-	"life_expectancy" NUMERIC(6,3),
-	"freedom" NUMERIC(6,3),
-	"Generosity" NUMERIC(6,3)
-);
-
-
-/* COPY hapiness
-FROM 'C:\code\sql\testimport\happiness.csv'
-DELIMITER ';'
-CSV Header;
-
-
-COPY hapiness
-FROM PROGRAM 'curl -s https://github.com/Mini-project-1-Group-3/Data-for-mini-project/blob/main/happiness_sorted.csv'
-DELIMITER ';'
-CSV Header; */
-
-
-COPY dim_happiness
-FROM PROGRAM 'curl -s https://raw.githubusercontent.com/Mini-project-1-Group-3/Data-for-mini-project/refs/heads/main/happiness_new.csv'
-DELIMITER ','
-CSV Header;
-
-
--- ALTER TABLE hapiness
--- ADD COLUMN happiness_id SERIAL PRIMARY KEY;
+DROP TABLE IF EXISTS dim_country, dim_happiness, dim_series, dim_gdp, indicators;
 
 
 CREATE TABLE IF NOT EXISTS dim_country (
@@ -47,6 +14,99 @@ COPY dim_country
 FROM PROGRAM 'curl -s https://raw.githubusercontent.com/Mini-project-1-Group-3/Data-for-mini-project/refs/heads/main/country_table_data.csv'
 DELIMITER ';'
 CSV Header;
+
+ALTER TABLE dim_country
+ADD COLUMN country_id SERIAL PRIMARY KEY;
+
+
+
+CREATE TABLE IF NOT EXISTS dim_happiness (
+	"country" VARCHAR(255),
+	"year" INT,
+	"log_gdp" NUMERIC(6,3),
+	"social_support" NUMERIC(6,3),
+	"life_expectancy" NUMERIC(6,3),
+	"freedom" NUMERIC(6,3),
+	"Generosity" NUMERIC(6,3)
+);
+
+COPY dim_happiness
+FROM PROGRAM 'curl -s https://raw.githubusercontent.com/Mini-project-1-Group-3/Data-for-mini-project/refs/heads/main/happiness_new.csv'
+DELIMITER ','
+CSV Header;
+
+
+ALTER TABLE dim_happiness
+ADD COLUMN country_id INT REFERENCES dim_country (country_id);
+
+UPDATE dim_country
+SET country_name = 'Laos'
+WHERE country_name = 'Lao PDR';
+
+UPDATE dim_country
+SET country_name = 'Syria'
+WHERE country_name = 'Syrian Arab Republic';
+
+UPDATE dim_country
+SET country_name = 'South Korea'
+WHERE country_name = 'Korea';
+
+UPDATE dim_country
+SET country_name = 'Kyrgyzstan'
+WHERE country_name = 'Kyrgyz Republic';
+
+UPDATE dim_country
+SET country_name = 'Turkiye'
+WHERE country_name = 'Türkiye';
+
+UPDATE dim_country
+SET country_name = 'Somaliland region'
+WHERE country_name = 'Somalia';
+
+UPDATE dim_country
+SET country_name = 'Gambia'
+WHERE country_name = 'The Gambia';
+
+UPDATE dim_country
+SET country_name = 'Slovakia'
+WHERE country_name = 'Slovak Republic';
+
+UPDATE dim_country
+SET country_name = 'Ivory Coast'
+WHERE country_name = 'Côte d''Ivoire';
+
+UPDATE dim_country
+SET country_name = 'Congo (Brazzaville)'
+WHERE country_name = 'Congo';
+
+UPDATE dim_country
+SET country_name = 'Hong Kong S.A.R. of China'
+WHERE country_name = 'Hong Kong SAR, China';
+
+UPDATE dim_happiness
+SET country_id = dim_country.country_id
+FROM dim_country
+WHERE dim_happiness.country = dim_country.country_name;
+
+
+
+CREATE TABLE IF NOT EXISTS dim_gdp (
+	country VARCHAR(255),
+	year INT,
+	value NUMERIC(20,15)
+);
+
+
+COPY dim_gdp
+FROM PROGRAM 'curl -s https://raw.githubusercontent.com/Mini-project-1-Group-3/Data-for-mini-project/refs/heads/main/debt_gdp_sorted.csv'
+DELIMITER ';'
+CSV Header;
+
+
+
+
+
+
 
 
 CREATE TABLE IF NOT EXISTS dim_series (
@@ -81,17 +141,7 @@ FROM PROGRAM 'curl -s https://raw.githubusercontent.com/Mini-project-1-Group-3/D
 DELIMITER ';'
 CSV Header;
 
-CREATE TABLE IF NOT EXISTS dim_gdp (
-	country VARCHAR(255),
-	year INT,
-	value NUMERIC(20,15)
-);
 
-
-COPY dim_gdp
-FROM PROGRAM 'curl -s https://raw.githubusercontent.com/Mini-project-1-Group-3/Data-for-mini-project/refs/heads/main/debt_gdp_sorted.csv'
-DELIMITER ';'
-CSV Header;
 
 
 SELECT *
